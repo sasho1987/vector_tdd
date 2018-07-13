01d4957906b9906fa46e26ad31d03905cd849009
Mansur <Alex.Mansur@McKesson.com>
2018-07-13 09:17:40 -0500

    New code by Alex

--- src/vector.py
+++ src/vector.py
@@ -1,4 +1,10 @@
 class Vector:
     def __init__(self, nums):
         self.nums = nums
+        self.dims = len(nums)
+        acc =1
 
+       
+
+    def _length(self):
+        return sum([c**2 for c in self.nums]) ** 0.5
--- test/test_vector.py
+++ test/test_vector.py
@@ -3,7 +3,15 @@ from src.vector import Vector
 def test_make_vector():
     v = Vector( [1,2,3])
     assert len(v.nums) == 3 #passes if true, flags if false
+
+
+def test_dimensions():
+    v = Vector( [6,7,8,9] )
+    assert v.dims == 4 
     
+def test_norm():
+    v = Vector( [3,4] )
+    assert v._length == 5
 
 
 
