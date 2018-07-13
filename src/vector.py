33853f30f8c397ca99e88a754ac1bc589c2947e7
Varner <Blake.Varner@McKesson.com>
2018-07-13 10:48:31 -0400

    Another Commit from BV

--- src/vector.py
+++ src/vector.py
@@ -2,9 +2,16 @@ class Vector:
     def __init__(self, nums):
         self.nums = nums
         self.dims = len(nums)
-        acc =1
+        self.norm = self._length()
 
-       
+    def __eq__(self, other):
+        return self.nums == other.nums
 
     def _length(self):
         return sum([c**2 for c in self.nums]) ** 0.5
+
+    def unit_vector(self):
+        return Vector([c / self.norm for c in self.nums])
+
+    def scale(self, _scale):
+        return Vector([c * _scale for c in self.nums])
